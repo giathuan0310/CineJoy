1355a04f9fde59b64d2be7cd703b8d486af7ca44
Ngo Nhut Tan <ngonhattan.sd1@gmail.com>
2025-07-02 20:21:25 +0700

    fix bug scroll to top

--- fe-cinejoy/src/components/chatBot/Chatbot.tsx
+++ fe-cinejoy/src/components/chatBot/Chatbot.tsx
@@ -213,7 +213,7 @@ const Chatbot: React.FC = () => {
             {showScrollTop && (
                 <button
                 onClick={handleScrollToTop}
-                className={`fixed ${isMenuOpen ? "bottom-60" : "bottom-40"} right-5 z-[9999] bg-blue-600 hover:bg-blue-800 text-white p-3.5 rounded-full shadow-lg transition-all duration-300 cursor-pointer`}
+                className={`fixed ${isMenuOpen ? "bottom-60" : "bottom-40"} right-5 ${isOpen ? "z-[9998]" : "z-[9999]"} bg-blue-600 hover:bg-blue-800 text-white p-3.5 rounded-full shadow-lg transition-all duration-300 cursor-pointer`}
                 title="Lên đầu trang"
                 >
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
